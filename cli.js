9aeb824b20ed3e83d02dd36b02eaf56276da47ed
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-11 22:31:41 -0800

    try fixing the path

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
 const byline = require('byline')
-const stream = byline(fs.createReadStream('titles', {encoding: 'utf8'}))
+const stream = byline(fs.createReadStream(path.join(__dirname, 'titles'), {encoding: 'utf8'}))
 const args = process.argv.slice(2)
 const query = args[0]
 
